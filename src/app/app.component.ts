bcb0015da4437de851cde162559ed107e18a2527
nikhil-1337 <nsavaliya17@gmail.com>
2018-10-28 22:51:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  animations: [
+    trigger("openClose", [
+      state(
+        "open",
+        style({
+          opacity: 1
+        })
+      ),
+      state(
+        "closed",
+        style({
+          opacity: 0
+        })
+      ),
+      transition("open => closed", [
+        
+        style({ "transform-origin": "right bottom 0", }),
+        animate("1500ms cubic-bezier(0,0.1,0.5,1)")]),
+
+      transition("closed => open", [
+        // style({ transform: "scale(-0.1,-0.5)" }),
+        style({ transform: "scale(0)", "transform-origin": "right bottom 0", }),
+        animate("1000ms cubic-bezier(0.5,0.1,0.5,1)")
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'chat-plugin';
+  title = "chat-plugin";
+  isOpen = false;
+
+  toogle() {
+    this.isOpen = !this.isOpen;
+  }
 }
