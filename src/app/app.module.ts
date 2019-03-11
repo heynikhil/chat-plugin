3de5915f1894796fd9476e48f88817755f601797
nikhil-1337 <nsavaliya17@gmail.com>
2019-03-11 19:58:59 +0530

    improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIcon } from "@angular/material";
 import { CookieService } from './cookie.service';
 @NgModule({
   declarations: [
-    MatIcon,
     AppComponent
   ],
   imports: [
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,15 @@
 export const environment = {
   production: true,
-  "type": "service_account",
-  "project_id": "esoteric-accord-232906",
-  "private_key_id": "9a4700e85b678cbf1d625ea01d6d15be7d7669c4",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCpaxdiJu7dD/bK\n2FimVSfMd8FWJZl0tpwTc76hHHYm7uByDtN06Gj18h8dNh4ybaN97rs3Pksr716A\n6ujQlFE8vhDvKiDOKkJYxKXUAhm87awtT09Zh4DqF9CnDPaG2nctD+W6bTccXkr4\ntR6drWqUl907xTD4v2i26TLmath9RjYNP1KSVzny5xnZfMo1ilgX96nZbFev8a5c\nI7WEJo+M1uIRP6PVhPgzEEPSv9gX+vs59553phxfkxtt2Gpzztcgww/SxsYTgJqM\nTnMEKgLkgzS4IpDSMVU8whFnIaP7JR1UCpTqXOWtEoNHYalsA4QfogBNkac/4eR3\ncSI/lAVtAgMBAAECggEAH2fNeiGu+lZFAMW5qz3SZp4VfidyqrbWOGnmnt2UjnOO\nMj6K1ttlZ6A2QddAIkd+PPBGDr/StgaFvX6uYUn5RDoUkA0+bxFY4rUza1lnVYU2\ne2C+HCHZFSwMfwcRZR625GjTxGsOAgl9utxyXDbjML/1ICkWAqqS2gNhLuMEFjah\nol7LKKsSNGJCacH63pm5v2Ft6mnm9S/C/LuFPrEikabCYTTIwLlm78vZVIkLVEuy\nOapl2Vu1PohHfU5mW2rcakJ0nbYd2719x7MZwT5gPvaqPe3u8eJUNL5cR45vK4qv\nY933Yn9zeDn0lpbLmuXCivOjwXneyT0NzGhjv5OcSQKBgQDsuSjRCWvA6wDYx7Nv\n9c3MXEe35BDc4oAJ0iEj/sJCXpo/sCJT5wBrYXgcOg+Od1tTERn1puqigHgGrdsV\n/49fWjSIV+JeCAKGIipRn9jTgrjgSOmHO45+HUt6X96gl7fDcym0qJO5BuE940ll\nC+PWHZ9ucFLHZu9Gut2iYC8COQKBgQC3NtzF5MSz5u11uRysn5zYlLn5eVSk/ggY\nE+PbNgllZvooBlfFvuxQ/aO93pZnQhqmL+FaHbkxXieHaYGyP/63fWsKhiqJy3/m\n7SjbWJvauQvwVwHaKgekZ/HXIFKTU8twh6VxKQOEv32620wILcu6yJxput0gNcCz\nZkrZnUCM1QKBgQCQKwARtuQZ/MVqQM0mTZQFdtnyw4IFlCgYUxH94wSZ+RWF3hY5\n9QCLZTQKNM4OnOBQiaS+t+ZEC2uN3coE7rx5fO1rb+KcE9K9sAcH6QrbXcyMQ7t9\nsabJZbv3DnX+Zgfzj6qBrP0yfV8mGsszL/eVCjtzvhKcADOPZvTeFCfs+QKBgBf9\n4S8w4+lDBjlyQiUAycM6xpe6obDxaxHsGNxt2REkSQHQwtGsTLnWXQjSCxP+fgrh\nHjt0xPmIANkkoc07wBk7HTdFlnBw65DL4ZvHoLElpkCUxDNTaajHy82F+Ao+4Ncd\njDajTkil4ssDSJORm6ob9w+JXseHAnL7pDWW+Ar5AoGAV0O80J7z+prZsOMVt9to\nRuim2QttROGt/0mX7h4FXWjt1pLcTKEXDdvv1NNHA+wmn2Sili8IdfzM6BC7vx6q\n5luKlp8Jbfoe6jW2qQbUwBMlZYexI2pXjXi5N9RJ14yZM+EmQoJD6a3rjaBkBKDu\nma02ULa6x6juwdVzJIFy+7c=\n-----END PRIVATE KEY-----\n",
-  "client_email": "dialogflow-gpwnqn@esoteric-accord-232906.iam.gserviceaccount.com",
-  "client_id": "102573417498986424119",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-gpwnqn%40esoteric-accord-232906.iam.gserviceaccount.com"
-};
+  
+    "type": "service_account",
+    "project_id": "plumbing-hvac-seo",
+    "private_key_id": "71a9ec6f0bcb321149ab7195737e3b2fe1e50960",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC79cej0OG6Hceu\nEB8W1UvZMd/sZ7jWosncI3RYz4j7iAMV7l283KGFcr7fyjDjacNXR3eq4alQW+fX\nYvTDLzMB+KQV6Y2dIPmaKsXBeQxPnpFSai52VqfW20Q0rbgEiIfmV8cQGkErZFZt\nHI4r6fl2kS15tYI1fuhh2Dh0ZdCF1SlStVsB8cekXZBr/Zk2gIYgos+lUZZEUka7\nCg4xqIzQdxPaEzuWaVVrZED770tf+nJXQjJ9+ycf/Czsmb/xH4hreiPAvDsQyO7x\nW8z0x2zQ0KqSPYbPWVbqytYV71nFHBBfOtHrwFOMGT4wSgXIz/VSterK7CNPs4s6\nLUoizr4xAgMBAAECggEAS54fyTn59iSQ9ugpecgqEaFNExDPfhpT16qz3h7wXzFi\n/Gm/cIHawZNeUy5G6Nbd7bkk3ayXr2qqJS78haHxhXOAoAgsHvDrOijHEoUefNGw\naVWqh5VpRLlGAWN6SuiatALQZRwK5+ggFRmN4QDqidbyUqrBTcBWwTZ5z+o+L/jW\n80+/sys9qo1zAF+cpMZjlDTFG//n/CKeP7kXN10nRz03DajQRtMVIx69pcPP7UEB\nwZK3Rs7n+CaU4f6DtCcJgBQElqrvzFC5DmvO1fFRC+KC2L3sM8PRFqVW05YHBdrZ\nWcSlX12Xn5iBikq+hI4rq001GAqofE/G2K+qEuTPGQKBgQDtJ6HxR20N3oGtajz9\n1Tfni21zF5KgTVjeNZpoZxDv+RUSObPVXH4LvcXCo8HzAqfNcW0SUsd82sAAyqzX\noS/KpWYGUwzMDyZDGLe1jth+31lMSSc6czLvzEb8VErm1M8ecutxN9AZRFx4T3C9\nzimt4QUVrK6rFuR7c2uvLmK7GwKBgQDK5WTTq2HIarIQCoYWeRTOZ+uJjzFHwcen\nOMHRylEYoiGcO0xejyyr+ZZUgtiGtQxvULW3i0j1zMYiTQqVImlJsiPWfABs75Ic\n7lzF10y2oFuNotp6j4hJEtcQ4LlMjG4ey1Yzv3bApdciA3NX3t+t51J9zGRDeHsk\nH3n7F7KUowKBgQDHpcnLWZoRRvRcjUdzWYCKyPIHJh0mHndl+jJXGg1EOKjogvNF\n92uVbtmBlmK8tcuWRJrSBiYykeV5xSXlEakuLDfLszbewxIvAiwGCI9xn42Jjzwb\nFMgY4BwSaNP1p8oFlDFpStdBri86iYYcA+dKnliteoZBOjB3TR+mhqJPaQKBgA2N\nyXCCcGPNDNwZd9+kcwle09M6p5YdkdLP7dKv6z70Bb7ED1P5yIgsgihfHUvSaGQ/\nRgRe357b8Rh1gRF5XQM5TT+UxQqB7obUPYpR2aQRjkwl5uZtFDOdeJR+DqLYdrrD\n5pxKXAp+iaKSDHCASPz7CP2yW7hOIhvFpoZpjNp1AoGBANgn+wSogMUBCA7223tb\nDY8IeVCe8pdFQliqwguwMs9rbaIJhCe2YIz3+bRweJlieiXJuxGEihW2UGUVP7Ch\nJsvM69g+oLpvG2UJGF/ch1lwKXaIi7nlAhR22C+kHt0In+USrlZxMFg5VNFdNEgO\n4lMRq4ECTrVmJHZMeNI7pvYu\n-----END PRIVATE KEY-----\n",
+    "client_email": "dialogflow-hllerg@plumbing-hvac-seo.iam.gserviceaccount.com",
+    "client_id": "115816940272633723092",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-hllerg%40plumbing-hvac-seo.iam.gserviceaccount.com"
+  
+  };
